51e7f7821079b5d5a805792636f47476b5c0a36e
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-22 12:44:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event.type=== 'DOMContentLoaded'){
+        document.querySelector('#text').textContent= 'This is really cool!'
+    }
+
+})
